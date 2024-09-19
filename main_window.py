394a67a3707da45608b8100ccc3e910483e7cbeb
MilkaDVP <gerbertshmidt@yandex.ru>
2024-09-19 15:31:51 +0700

    new files
    ui files and update py files

--- main_window.py
+++ main_window.py
@@ -11,17 +11,23 @@ class MainWindow(QMainWindow):
 
       self.exit_button.clicked.connect(self.exit)
       self.camera_button.clicked.connect(self.camera_capture)
+      self.settings_button.clicked.connect(self.settings)
 
    def exit(self):
       sys.exit()
 
    def camera_capture(self):
-      Popen('python naebalovo.py')
+      Popen('python camera_select.py')
+
+   def settings(self):
+      Popen('python settings.py')
 
 
 
 if __name__ == '__main__':
    app = QApplication(sys.argv)
    form = MainWindow()
+   form.setFixedSize(590, 550)
+   form.setWindowTitle('Главное окно')
    form.show()
    app.exec()
---
+++ settings.py
@@ -0,0 +1,18 @@
+import sys
+from PyQt5 import uic
+from PyQt5.QtWidgets import QMainWindow, QApplication
+
+
+class MainWindow(QMainWindow):
+   def __init__(self):
+      super().__init__()
+      uic.loadUi('ui files/settings.ui', self)
+
+
+if __name__ == '__main__':
+   app = QApplication(sys.argv)
+   form = MainWindow()
+   form.setFixedSize(830, 260)
+   form.setWindowTitle('Настройки')
+   form.show()
+   app.exec()
